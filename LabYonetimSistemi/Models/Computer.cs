c786664e1a1e7275be12f96bf51f701935a7bc0d
Ömer Avcı <execoder.u@gmail.com>
2026-05-13 11:54:37 +0300

    hafta-3: Lambda, GET ve POST endpointleri eklendi

--- LabYonetimSistemi/Models/Computer.cs
+++ LabYonetimSistemi/Models/Computer.cs
@@ -3,9 +3,8 @@
 public class Computer
 {
     public int Id { get; set; }
-    public string Marka { get; set; } = string.Empty;
+    public string Brand { get; set; } = string.Empty;
     public int Ram { get; set; }
-    public bool BozukMu { get; set; }
-
+    public bool HasIssue { get; set; }
     public string Processor { get; set; } = string.Empty;
 }
--- LabYonetimSistemi/Program.cs
+++ LabYonetimSistemi/Program.cs
@@ -1,17 +1,34 @@
 using LabYonetimSistemi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-var computers = new List<Computer>
+app.UseSwagger();
+app.UseSwaggerUI();
+
+var pcListesi = new List<Computer>
 {
-    new Computer { Id = 1, Marka = "Dell", Ram = 16, BozukMu = false, Processor = "Intel" },
-    new Computer { Id = 2, Marka = "HP", Ram = 8, BozukMu = true, Processor = "AMD" },
-    new Computer { Id = 5, Marka = "Acer", Ram = 8, BozukMu = true, Processor = "Intel" },
-    new Computer { Id = 3, Marka = "Lenovo", Ram = 32, BozukMu = false, Processor = "Intel" },
-    new Computer { Id = 4, Marka = "Asus", Ram = 16, BozukMu = false, Processor = "AMD" }
+    new Computer { Id = 1, Brand = "Monster", Ram = 16, HasIssue = false },
+    new Computer { Id = 2, Brand = "Lenovo", Ram = 8, HasIssue = true }
 };
 
-app.MapGet("/api/pc", () => computers);
+app.MapGet("/api/bilgisayarlar", () => pcListesi);
 
-app.Run();
+app.MapGet("/api/arizali-pcler", () =>
+    pcListesi.Where(x => x.HasIssue == true).ToList());
+
+app.MapGet("/api/bilgisayar/{id}", (int id) => {
+    var bulunan = pcListesi.Find(x => x.Id == id);
+    return bulunan;
+});
+
+app.MapPost("/api/bilgisayar-ekle", (Computer yeniPc) => {
+    pcListesi.Add(yeniPc);
+    return $"Yeni bilgisayar ({yeniPc.Brand}) baþarýyla eklendi!";
+});
+
+app.Run();  
