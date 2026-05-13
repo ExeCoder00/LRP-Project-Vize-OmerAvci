74fbfa73e772288d36f9f37cda9d9d09001baa40
Ömer Avcı <execoder.u@gmail.com>
2026-05-13 11:08:28 +0300

    Hafta 2: Bilgisayar modeli ve listesi eklendi

---
+++ LabYonetimSistemi/Models/Computer.cs
@@ -0,0 +1,11 @@
+﻿namespace LabYonetimSistemi.Models;
+
+public class Computer
+{
+    public int Id { get; set; }
+    public string Marka { get; set; } = string.Empty;
+    public int Ram { get; set; }
+    public bool BozukMu { get; set; }
+
+    public string Processor { get; set; } = string.Empty;
+}
--- LabYonetimSistemi/Program.cs
+++ LabYonetimSistemi/Program.cs
@@ -1,5 +1,17 @@
+using LabYonetimSistemi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build(); 
-app.MapGet("/hosgeldiniz", () => "Laboratuvar Takip Sistemine Hoş Geldiniz!");
+var app = builder.Build();
+
+var computers = new List<Computer>
+{
+    new Computer { Id = 1, Marka = "Dell", Ram = 16, BozukMu = false, Processor = "Intel" },
+    new Computer { Id = 2, Marka = "HP", Ram = 8, BozukMu = true, Processor = "AMD" },
+    new Computer { Id = 5, Marka = "Acer", Ram = 8, BozukMu = true, Processor = "Intel" },
+    new Computer { Id = 3, Marka = "Lenovo", Ram = 32, BozukMu = false, Processor = "Intel" },
+    new Computer { Id = 4, Marka = "Asus", Ram = 16, BozukMu = false, Processor = "AMD" }
+};
+
+app.MapGet("/api/pc", () => computers);
 
 app.Run();
